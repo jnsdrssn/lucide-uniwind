00582c6478f4ffcd60c14d6d505e9e5ee54e28e6
Jonas Derissen <jonas.derissen@unitybox.de>
2025-11-11 13:09:59 -0800

    feat: replace nativewind with uniwind

--- README.md
+++ README.md
@@ -1,51 +1,59 @@
-# Lucide Icons for React Native with NativeWind
-  [![npm](https://img.shields.io/npm/v/lucide-nativewind?color=blue)](https://www.npmjs.com/package/lucide-nativewind)
-  ![NPM Downloads](https://img.shields.io/npm/dw/lucide-nativewind)
-  [![GitHub](https://img.shields.io/github/license/lucide-icons/lucide)](https://lucide.dev/license)
-  
-A drop in replacement for `lucide-react-native` to work with NativeWind. It provides [Lucide icons](https://lucide.dev/) with an additional `className` prop to allow for easy integration with [NativeWind](https://www.nativewind.dev/).
+# Lucide Icons for React Native with Uniwind
+
+[![npm](https://img.shields.io/npm/v/lucide-nativewind?color=blue)](https://www.npmjs.com/package/lucide-nativewind)
+![NPM Downloads](https://img.shields.io/npm/dw/lucide-nativewind)
+[![GitHub](https://img.shields.io/github/license/lucide-icons/lucide)](https://lucide.dev/license)
+
+A drop in replacement for `lucide-react-native` to work with Uniwind. It provides [Lucide icons](https://lucide.dev/) with an additional `className` prop to allow for easy integration with [Uniwind](https://docs.uniwind.dev/).
 
 ## Installation
+
 You can use either npm or yarn:
 
 Using npm:
 
 ```bash
-npm install lucide-nativewind
+npm install lucide-uniwind
 ```
+
 Using yarn:
 
 ```bash
-yarn add lucide-nativewind
+yarn add lucide-uniwind
 ```
-## Usage
-`lucide-nativewind` is built with ES modules so it's completely tree-shakable. Each icon can be imported as a react component.
 
+## Usage
+
+`lucide-uniwind` is built with ES modules so it's completely tree-shakable. Each icon can be imported as a react component.
 
 All icons can be imported the same as if using the `lucide-react-native` package
+
 ```diff
 - import { ArrowLeftCircleIcon } from 'lucide-react-native';
-+ import { ArrowLeftCircleIcon } from 'lucide-nativewind';
++ import { ArrowLeftCircleIcon } from 'lucide-uniwind';
 ```
 
 ## Custom Icons
+
 Custom icons can be wrapped with `iconWithClassName` to add the `className` prop.
 
 ```jsx
-import { iconWithClassName } from 'lucide-nativewind';
-import MyCustomIcon from './MyCustomIcon';
+import { iconWithClassName } from "lucide-uniwind";
+import MyCustomIcon from "./MyCustomIcon";
 
 const MyCustomIconWithClassName = iconWithClassName(MyCustomIcon);
 ```
 
 ## Versioning
+
 The version is automatically kept in sync with `lucide-react-native`
 
 ## Contributing
+
 All contributions welcome. Contributions/requests for new/updated icons should be directed to the lucide project: https://github.com/lucide-icons/lucide
 
 ## FAQs
 
 ### What icons are available?
-All lucide icons are available, and this package will be updated whenever new icons are added to lucide. This package is not intended to add any additional or custom icons.
 
+All lucide icons are available, and this package will be updated whenever new icons are added to lucide. This package is not intended to add any additional or custom icons.
--- scripts/generateIcons.ts
+++ scripts/generateIcons.ts
@@ -1,72 +1,93 @@
-import {icons} from "lucide";
-import {writeFileSync, mkdirSync, rmSync} from "fs";
+import { mkdirSync, rmSync, writeFileSync } from "fs";
+import { icons } from "lucide";
 
 type SVGProps = Record<string, string | number>;
 type IconNodeChild = readonly [tag: string, attrs: SVGProps];
-type IconNode = readonly [tag: string, attrs: SVGProps, children?: IconNodeChild[]];
+type IconNode = readonly [
+  tag: string,
+  attrs: SVGProps,
+  children?: IconNodeChild[]
+];
 
-const toKebabCase = (string: string) => string.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+const toKebabCase = (string: string) =>
+  string.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
 
-const toCamelCase = (string: string) => string.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+const toCamelCase = (string: string) =>
+  string.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
 
 const iconDataToReact = (icon: IconNode | IconNodeChild): string => {
-    const props = Object.entries(icon[1]).map(([prop, value])=> `${toCamelCase(prop)}={${JSON.stringify(value)}}`).join(" ")
-    if(icon[2] === undefined){
-        return `<${icon[0]} ${props}/>`
-    } else {
-        return `<${icon[0]} ${props}>${icon[2].map(iconDataToReact).join("")}</${icon[0]}>`
-    }
-
-}
+  const props = Object.entries(icon[1])
+    .map(([prop, value]) => `${toCamelCase(prop)}={${JSON.stringify(value)}}`)
+    .join(" ");
+  if (icon[2] === undefined) {
+    return `<${icon[0]} ${props}/>`;
+  } else {
+    return `<${icon[0]} ${props}>${icon[2].map(iconDataToReact).join("")}</${
+      icon[0]
+    }>`;
+  }
+};
 
 // The path to generate icons to
 const ICON_PATH = "icons";
 
 // start by deleting the src directory (if it exists)
-rmSync(`src/${ICON_PATH}`, {recursive: true, force: true});
+rmSync(`src/${ICON_PATH}`, { recursive: true, force: true });
 
-mkdirSync(`src/${ICON_PATH}`, {recursive: true});
+mkdirSync(`src/${ICON_PATH}`, { recursive: true });
 
 Object.entries(icons).forEach(([iconName, iconContent]) => {
-
   const iconNameLowerCase = toKebabCase(iconName);
 
-    // filename is icon.ts
-    const filename = `src/${ICON_PATH}/${iconNameLowerCase}.tsx`;
+  // filename is icon.ts
+  const filename = `src/${ICON_PATH}/${iconNameLowerCase}.tsx`;
 
-
-    const fileContent = `import { ${iconName} } from "lucide-react-native";
+  const fileContent = `import { ${iconName} } from "lucide-react-native";
 import iconWithClassName from '../iconWithClassName';
 /**
- *  [${iconName} on lucide.dev](https://lucide.dev/icons/${toKebabCase(iconName)})
+ *  [${iconName} on lucide.dev](https://lucide.dev/icons/${toKebabCase(
+    iconName
+  )})
  */    
 
 export default iconWithClassName(${iconName});
-`
-    // write the file to disk
-    writeFileSync(filename, fileContent);
+`;
+  // write the file to disk
+  writeFileSync(filename, fileContent);
 });
 
 // Write a seperate web export
 Object.entries(icons).forEach(([iconName, iconContent]) => {
+  const iconNameLowerCase = toKebabCase(iconName);
 
-    const iconNameLowerCase = toKebabCase(iconName);
+  // filename is icon.ts
+  const filename = `src/${ICON_PATH}/${iconNameLowerCase}.web.tsx`;
 
-    // filename is icon.ts
-    const filename = `src/${ICON_PATH}/${iconNameLowerCase}.web.tsx`;
-
-
-    const fileContent = `import { ${iconName} } from "lucide-react";\nexport default ${iconName};`
-    // write the file to disk
-    writeFileSync(filename, fileContent);
+  const fileContent = `import { ${iconName} } from "lucide-react";\nexport default ${iconName};`;
+  // write the file to disk
+  writeFileSync(filename, fileContent);
 });
 
 // generate barrel file
-const barrelExports = Object.entries(icons).map(([iconName]) => `export { default as ${iconName}Icon, default as Lucide${iconName} } from './icons/${toKebabCase(iconName)}';`);
-const barrelExportsWeb = Object.entries(icons).map(([iconName]) => `export { default as ${iconName}Icon, default as Lucide${iconName} } from './icons/${toKebabCase(iconName)}.web';`);
+const barrelExports = Object.entries(icons).map(
+  ([iconName]) =>
+    `export { default as ${iconName}Icon, default as Lucide${iconName} } from './icons/${toKebabCase(
+      iconName
+    )}';`
+);
+const barrelExportsWeb = Object.entries(icons).map(
+  ([iconName]) =>
+    `export { default as ${iconName}Icon, default as Lucide${iconName} } from './icons/${toKebabCase(
+      iconName
+    )}.web';`
+);
 
-const extraBarrelExports = [`export type { LucideIcon, LucideProps } from 'lucide-react-native'`, `export type { LucidePropsWithClassName } from './iconWithClassName'`,  `export { default as iconWithClassName } from './iconWithClassName'`]
-barrelExports.push(...extraBarrelExports)
+const extraBarrelExports = [
+  `export type { LucideIcon, LucideProps } from 'lucide-react-native'`,
+  `export type { LucidePropsWithClassName } from './iconWithClassName'`,
+  `export { default as iconWithClassName } from './iconWithClassName'`,
+];
+barrelExports.push(...extraBarrelExports);
 
 writeFileSync(`src/index.ts`, barrelExports.join("\n"));
 writeFileSync(`src/index.web.ts`, barrelExportsWeb.join("\n"));
@@ -74,28 +95,17 @@ writeFileSync(`src/index.web.ts`, barrelExportsWeb.join("\n"));
 // generate iconWithClassName file
 const iconWithClassNameFile = `import {ReactNode} from 'react';
 import type { LucideProps } from 'lucide-react-native';
-import { cssInterop } from 'nativewind';
+import { withUniwind } from 'uniwind';
 
 export type LucidePropsWithClassName = LucideProps & {
     className?: string;
 }
 
 /**
- * Helper function that wraps a LucideIcon with \`cssInterop\` to allow for styling with the \`className\` prop
+ * Helper function that wraps a LucideIcon with \`withUniwind\` to allow for styling with the \`className\` prop
  */
 export default function iconWithClassName(icon: (props: LucideProps) => ReactNode): (props: LucidePropsWithClassName) => ReactNode {
-  return cssInterop(icon, {
-    className: {
-      target: 'style',
-      nativeStyleToProp: {
-        color: true,
-        opacity: true,
-        width: true,
-        height: true,
-      },
-    },
-  }) as any;
-}`
-
+  return withUniwind(icon);
+}`;
 
 writeFileSync(`src/iconWithClassName.ts`, iconWithClassNameFile);
